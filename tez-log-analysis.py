47b0cc3db2dd993afcc4e3e2ae7edee70b4cbdff
rp <rahulpathak109@gmail.com>
2021-09-02 12:45:57 -0700

    v2.1

--- tez-log-analysis.py
+++ tez-log-analysis.py
@@ -12,32 +12,31 @@ LOCAL_AGGREGATION = "LogAggregationType: LOCAL"
 DAG_LOG_REGEX = "LogType:syslog_dag_\d+_\d+_\d+$"
 DOT_LOG_REGEX = "LogType:dag_\d+_\d+_\d+_priority.dot"
 CONTAINER_PREFIX = 'Container: '
-LOGTYPE_PREFIX = 'LogType:'
-LOGTYPE_SEPARATOR = ':'
+LOGTYPE_PREFIX = "LogType:"
+LOGTYPE_SEPARATOR = ":"
 LOGTYPE_END = 'End of LogType:'
 
 
-def remove_and_create(dir):
+def remove_and_create(log_dir):
     try:
-        mkdir(dir)
+        mkdir(log_dir)
     except OSError:
-        rmtree(dir)
-        mkdir(dir)
+        rmtree(log_dir)
+        mkdir(log_dir)
 
 
-def split_logs(log, outputdir):
-    outputdir = path.abspath(outputdir)
+def split_logs(log, output_dir):
+    output_dir = path.abspath(output_dir)
     try:
-        makedirs(outputdir)
+        makedirs(output_dir)
     except OSError:
         pass
-    containers_base = path.join(outputdir, 'containers')
-    hosts_base = path.join(outputdir, 'hosts')
+    containers_base = path.join(output_dir, 'containers')
+    hosts_base = path.join(output_dir, 'hosts')
     remove_and_create(containers_base)
     remove_and_create(hosts_base)
     containers = set()
     hosts = set()
-    container = None
     split_file = None
     container_dir = None
     logtype = None
@@ -93,28 +92,51 @@ def grep_line(file_path, regex):
 
 def analyze_log(dag_log, logs_list):
     tasks_list = grep_line(dag_log, re.compile("TASK_ATTEMPT_FINIS"))
-    task_details = []
-    for task in tasks_list:
-        task_id = task.split(",")[2].split("=")[1]
-        task_status = task.split(",")[8].split("=")[1]
-        create_time = int(task.split(",")[3].split("=")[1])
-        start_time = int(task.split(",")[5].split("=")[1])
-        wait_time = start_time - create_time
-        run_time = int(task.split(",")[7].split("=")[1])
-        task_details.append((wait_time, run_time, task_id, task_status))
-    print "Total tasks in dag = {}".format(len(task_details))
-    top_runtime = sorted(task_details, key=lambda x: x[1])[-1]
-    top_wait = sorted(task_details)[-1]
-    print "Longest run time of {} seconds was taken by {} with status " \
-          "{}".format(top_runtime[1] / 1000, top_runtime[2], top_runtime[3])
-    print "Longest wait time of {} seconds was taken by {} with status " \
-          "{}".format(top_wait[0] / 1000, top_wait[2], top_wait[3])
-    long_log_regex = "LogType:syslog_{}".format(top_runtime[2])
-    long_log_file = []
-    for file_path in logs_list:
-        if grep_line(file_path, re.compile(long_log_regex)):
-            long_log_file.append(file_path)
-    print "\nCheck below log for more details:\n{}\n{}\n".format(long_log_file[-1], dag_log)
+    if len(tasks_list) > 0:
+        print "Total tasks in dag = {}".format(len(tasks_list))
+        task_ok = []
+        task_failed = []
+        for task in tasks_list:
+            task_id = task.split(",")[2].split("=")[1]
+            task_status = task.split(",")[8].split("=")[1]
+            wait_time = int(task.split(",")[5].split("=")[1]) - int(task.split(",")[3].split("=")[1])
+            run_time = int(task.split(",")[7].split("=")[1])
+            if task_status == 'SUCCEEDED':
+                task_ok.append((wait_time, run_time, task_id, task_status))
+            elif task_status == 'FAILED':
+                task_failed.append((wait_time, run_time, task_id, task_status))
+        top_runtime = []
+        top_wait = []
+        if len(task_ok) > 0:
+            top_runtime = sorted(task_ok, key=lambda x: x[1])[-1]
+            top_wait = sorted(task_ok)[-1]
+        elif len(task_ok) == 0 and len(task_failed) == 0:
+            print "No failed or succeeded task found. Check below log for details.\n{}".format(dag_log)
+            return
+        long_log_file = []
+        if len(top_wait) > 0 and len(top_runtime) > 0:
+            print "Longest run time of {} seconds was taken by {} with status " \
+                  "{}".format(top_runtime[1] / 1000, top_runtime[2], top_runtime[3])
+            print "Longest wait time of {} seconds was taken by {} with status " \
+                  "{}".format(top_wait[0] / 1000, top_wait[2], top_wait[3])
+            log_regex = "LogType:syslog_{}".format(top_runtime[2])
+            for file_path in logs_list:
+                if grep_line(file_path, re.compile(log_regex)):
+                    long_log_file.append(file_path)
+        if len(task_failed) > 0:
+            for items in task_failed:
+                print "Found failure for task {}".format(items[2])
+                log_regex = "LogType:syslog_{}".format(items[2])
+                for file_path in logs_list:
+                    if grep_line(file_path, re.compile(log_regex)):
+                        print "Log location: {}".format(file_path)
+        if len(long_log_file) > 0:
+            print "\nCheck below logs for details about long running and waiting " \
+                  "tasks:\n{}\n{}\n".format(long_log_file[-1], dag_log)
+        else:
+            print "\n\tDag log location:\n{}".format(dag_log)
+    else:
+        print "No task found in dag.\nCheck below log for details.\n{}".format(dag_log)
 
 
 def print_viz(dag_log_dot):
@@ -153,10 +175,9 @@ if __name__ == '__main__':
                     analyze_log(dag_files[args.dagid - 1], all_files)
                 else:
                     print "\nAnalyzing dag {}".format(dag_files[0].split('/')[3])
-                    print "\n\tNote : More than one dag found and either dagid option was", \
-                        "\n\t      not used or dag with given id in option not found.", \
-                        "\n\tTo check specific dag rerun script with", \
-                        "--dagid [1-{}]".format(dag_count)
+                    print "\n\tNote: Total {} dags found.".format(dag_count), \
+                        "\n\tEither dagid option was not used or dag with given id in option not found.", \
+                        "\n\tTo check specific dag rerun script with --dagid <dag-number>"
                     print "\tExample:\n\tpython", sys.argv[0], "--logs <log_file> --dagid 1\n"
                     analyze_log(dag_files[0], all_files)
             else:
